acd119d4e5bb823a3ea18c995474bb7141c99b37
Shohei Kai <shoheikai@mbp-kai.local>
2020-01-24 17:23:51 +0900

    divide add repository method, move to repository

--- src/domain/MeetingRoom.php
+++ src/domain/MeetingRoom.php
@@ -4,9 +4,7 @@ namespace MeetingRoomReservationModeling;
 
 class MeetingRoom
 {
-    private $name;
-
-    private static $reservations = [];
+    public $name;
 
     public function __construct(string $name)
     {
@@ -19,30 +17,4 @@ class MeetingRoom
         return $reservation;
     }
 
-    public static function addReservation(Reservation $reservation)
-    {
-        if (self::existsOverlappedReservation($reservation)) {
-            throw new \RuntimeException();
-        }
-
-        $key = $reservation->room->name;
-        if (!isset(self::$reservations[$key])) {
-            self::$reservations[$key] = [];
-        }
-        self::$reservations[$key][] = $reservation;
-    }
-
-    public static function existsOverlappedReservation(Reservation $reservation): bool
-    {
-        if (!isset(self::$reservations[$reservation->room->name])) {
-            return false;
-        }
-        foreach (self::$reservations[$reservation->room->name] as $aReservation) {
-            /** @var Reservation $aReservation */
-            if ($aReservation->range->overlapped($reservation->range)) {
-                return true;
-            }
-        }
-        return false;
-    }
 }
